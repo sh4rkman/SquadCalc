a3a0c1aef54002827b1d9ea872d1c3d8d58fd26c
Maxime Boussard <sh4rkman@users.noreply.github.com>
2020-09-29 01:31:20 +0200

    Fixed bashrah z-scaling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Each map has a different size and require scaling w, y and z when calculating height
 // Map['Name', size, z-scaling]
 var maps = [
-  ['Al Basrah', 3200, 0.47],
+  ['Al Basrah', 3200, 0.4],
   ['Belaya', 3904, 0.5],
   ['Chora', 4064, 0.06],
   ['FoolsRoad', 1736, 0.149],
