a0a7e11b2440bbaa301af302c1a22a34d2bee5a4
Maxime Boussard <sh4rkman@users.noreply.github.com>
2021-06-12 13:00:07 +0200

    Attemp to fix #23

--- js/utils.js
+++ js/utils.js
@@ -267,6 +267,10 @@ function loadHeatmap() {
     // Get Keypads and format it
     a = $("#mortar-location").val();
     b = $("#target-location").val();
+    
+    $("#mortar-location").val('');
+    $("#target-location").val('');
+
     $("#mortar-location").val(formatKeyPad(a));
     $("#target-location").val(formatKeyPad(b));
   
