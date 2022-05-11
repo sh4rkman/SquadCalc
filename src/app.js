49dc77a1cc2d65b4938864f060383626b4da3dea
Maxime Boussard <sh4rkman@users.noreply.github.com>
2022-05-11 19:20:23 +0200

    Giving up IE support
    redirect IE users to choosebrowser.com

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ import './css/bluetheme.scss';
 import './css/greentheme.scss';
 import './css/styles.scss';
 import './css/responsive.scss';
-import { loadHeatmap } from "./js/utils.js";
-import { loadMaps } from "./js/utils.js";
-import { preventAutocomplete, getTheme } from "./js/utils.js";
-
-
+import { loadHeatmap, loadMaps, preventAutocomplete, getTheme } from "./js/utils.js";
 
 
 $(function() {
