8c4bfde6c7b6966acc9e0d6642d584d6aabc801e
sh4rkman <sh4rkman@users.noreply.github.com>
2024-03-20 23:45:16 +0100

    favicon/pwa
    - new webpack plugin to generate manifest/favicon

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const RobotstxtPlugin = require("robotstxt-webpack-plugin");
-const WebpackPwaManifest = require('webpack-pwa-manifest')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -19,42 +19,18 @@ module.exports = merge(common, {
             }
         }),
         new RobotstxtPlugin({ policy: [{ userAgent: "*", allow: "/", }] }),
-        new WebpackPwaManifest({
-            name: 'SquadCalc',
-            short_name: 'SquadCalc',
-            description: 'A Minimalist Squad Mortar Calculator',
+        new FaviconsWebpackPlugin({
+            logo: './src/img/favicons/favicon.png', // svg works too!
+            publicPath: '/dist',
+            prefix: './src/img/favicons/',
+            inject: true,
             start_url: "https://squadcalc.app/",
-            background_color: '#111111',
-            theme_color: '#111111',
-            crossorigin: 'use-credentials',
-            icons: [{
-                    // Default .ico
-                    src: path.resolve('src/img/favicons/favicon.ico'),
-                    sizes: [16, 32],
-                },
-                {
-                    // General use iOS/Android
-                    src: path.resolve('src/img/favicons/favicon.png'),
-                    size: 180,
-                    destination: path.join('src', 'img', 'favicons'),
-                    ios: true,
-                },
-                {
-                    // 
-                    src: path.resolve('src/img/favicons/favicon.png'),
-                    sizes: [32, 57, 76, 96, 120, 128, 144, 152, 195, 196],
-                    destination: path.join('src', 'img', 'favicons'),
-                },
-                {
-                    // Mask for Safari
-                    src: path.resolve('src/img/favicons/favicon.svg'),
-                    size: '1024x1024',
-                    destination: path.join('src', 'img', 'favicons'),
-                    purpose: 'maskable'
-                }
-            ]
-        }),
-
+            favicons: {
+              appName: 'SquadCalc',
+              appDescription: 'A Minimalist Squad Mortar Calculator',
+              manifestMaskable: true,
+            }
+          }),
     ],
     module: {},
     optimization: {
