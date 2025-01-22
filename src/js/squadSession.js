2a35bc9c8dda844f1df78921916aba42faa4b36b
sh4rkman <maxime.boussard@gmail.com>
2025-01-22 21:52:27 +0100

    Update squadSession.js

--- src/js/squadSession.js
+++ src/js/squadSession.js
@@ -8,7 +8,7 @@ export default class SquadSession {
 
     constructor(sessionId) {
         const WS_URL = process.env.API_URL.replace(/^http/, "ws");
-        console.log(WS_URL)
+        console.log(WS_URL+"/")
         this.ws = new WebSocket(WS_URL);
         this.wsActiveUsers = 1;
 
