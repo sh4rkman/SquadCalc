ac875237442653bc913ea5f7f97e7ce895c1d0a3
yobange <recum12233@mail.ru>
2026-06-16 17:59:54 +0300

    Remove unused heightmapPNG scale configuration from maps.js

--- src/data/maps.js
+++ src/data/maps.js
@@ -465,9 +465,6 @@ export const MAPS = [
                 size: [6351, 6351],
                 scale: [1, 1, 4],
                 BWlevels: [0, 833]
-            },
-            heightmapPNG: {
-                scale: [1, 1, 0.3],
             }
         }
     },
